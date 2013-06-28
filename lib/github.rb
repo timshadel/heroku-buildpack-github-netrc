bb727f6ea6caad5a3598acf11344ea893a8c153c
Tim Shadel <tim@shadelsoftware.com>
2013-06-28 16:43:01 -0600

    Remove authorization info
    That's not accessible without original user/pass.

--- lib/github.rb
+++ lib/github.rb
@@ -14,15 +14,7 @@ def github_user_login token
   user["login"] || "error"
 end
 
-def github_user_auth token
-  # This call must use .netrc user/pass combination
-  json = `curl -s https://api.github.com/authorizations` rescue "[]"
-  all = JSON.parse json rescue []
-  authz = all.select {|a| a["token"] =~ /#{token}/}.first || []
-  "#{authz["note"]} (#{authz["scopes"].include?('repo') ? 'private repo access' : 'insufficient access'})"
-end
-
-def github_user_orgs
+def github_user_orgs token
   json = `curl -H "Authorization: Bearer #{token}" -s https://api.github.com/user/orgs` rescue "[]"
   orgs = JSON.parse json rescue []
   orgs.map {|o| o["login"].downcase }.sort
@@ -31,7 +23,6 @@ end
 def user_block token
   <<-USER
        Github User:   #{login(token)}
-       Authorization: #{github_user_auth(token)}
        Organizations: #{github_user_orgs(token).join(", ")}
   USER
 end

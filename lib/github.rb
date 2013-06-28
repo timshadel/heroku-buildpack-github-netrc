cb567774500931aa2955cb35e1283d6f64107d2b
Tim Shadel <tim@shadelsoftware.com>
2013-06-28 16:28:42 -0600

    Switch to bearer token

--- lib/github.rb
+++ lib/github.rb
@@ -1,36 +1,37 @@
 require 'json'
 
-def valid_login?
-  login != "error"
+def valid_login? token
+  login(token) != "error"
 end
 
-def login
-  @login ||= github_user_login
+def login token
+  @login ||= github_user_login(token)
 end
 
-def github_user_login
-  json = `curl -s https://api.github.com/user` rescue "{}"
+def github_user_login token
+  json = `curl -H "Authorization: Bearer #{token}" -s https://api.github.com/user` rescue "{}"
   user = JSON.parse json rescue {}
   user["login"] || "error"
 end
 
 def github_user_auth token
+  # This call must use .netrc user/pass combination
   json = `curl -s https://api.github.com/authorizations` rescue "[]"
   all = JSON.parse json rescue []
-  authz = all.select {|a| a["token"] =~ /#{token}/}.first
+  authz = all.select {|a| a["token"] =~ /#{token}/}.first || []
   "#{authz["note"]} (#{authz["scopes"].include?('repo') ? 'private repo access' : 'insufficient access'})"
 end
 
 def github_user_orgs
-  json = `curl -s https://api.github.com/user/orgs` rescue "[]"
+  json = `curl -H "Authorization: Bearer #{token}" -s https://api.github.com/user/orgs` rescue "[]"
   orgs = JSON.parse json rescue []
   orgs.map {|o| o["login"].downcase }.sort
 end
 
 def user_block token
   <<-USER
-       Github User:   #{login}
+       Github User:   #{login(token)}
        Authorization: #{github_user_auth(token)}
-       Organizations: #{github_user_orgs.join(", ")}
+       Organizations: #{github_user_orgs(token).join(", ")}
   USER
 end
